2bb37eace2fba29fdfefa5836e952e8b7d3e85b4
GettyOrawo <gettyorawo@gmail.com>
2020-02-16 01:09:42 +0300

    implement locate endpoint

--- lib/carpool_api.ex
+++ lib/carpool_api.ex
@@ -7,6 +7,8 @@ defmodule CarpoolApi do
   if it comes from the database, an external API or others.
   """
 
+  alias CarpoolApi.Cache
+
   @doc """
   Validates all payload formats for cars before save
   """
@@ -25,17 +27,17 @@ defmodule CarpoolApi do
   and that no other keys are passed in
   """
 
-  def filter_by_keys_and_length(params) do
+  def filter_by_keys_and_length(cars_list) do
 
-    Enum.map(params, fn param -> 
-      params = convert_keys_to_atoms(param)
+    Enum.map(cars_list, fn vehicle -> 
+      car_map = convert_keys_to_atoms(vehicle)
 
-      case Map.has_key?(params, :id) and Map.has_key?(params, :seats) do
+      case Map.has_key?(car_map, :id) and Map.has_key?(car_map, :seats) do
         true -> 
-          case Enum.count(params) == 2 and is_integer(params.seats) do
+          case Enum.count(car_map) == 2 and is_integer(car_map.seats) do
             true ->
-              CarpoolApi.Cache.delete_all()
-              CarpoolApi.Cache.put_cars(:cars, params)
+              Cache.delete_all()
+              Cache.put_cars(:cars, cars_list)
             false -> false
           end
         false -> false
@@ -53,7 +55,7 @@ defmodule CarpoolApi do
       true -> 
         case Enum.count(params) == 2 and is_integer(params.people) do
           true -> 
-            CarpoolApi.Cache.add_group(params.id, params)
+            Cache.add_group(params.id, params)
           false -> false
         end
       false -> false
@@ -63,34 +65,36 @@ defmodule CarpoolApi do
   @doc """
   Validates incomming id formats before deregistering valid group
   """
-  def validate_group_id_and_deregister(group_id), when is_integer(group_id) do
+  def validate_group_id_and_deregister(group_id) when is_integer(group_id) do
     case record_exists?(:group_cache, group_id) do
       nil -> "no record"
       _group -> 
-        CarpoolApi.Cache.delete_group(group_id)
+        Cache.delete_group(group_id)
     end
   end
 
-    def validate_group_id_and_deregister(_group_id) do
-      false
-    end
+  def validate_group_id_and_deregister(_group_id) do
+    false
   end
 
   @doc """
   Tries to find a car for the incomming group
   """
-  def find_car(group, cars), when record_exists?(table, key) and is_integer(group.id) do
-    case Enum.filter(cars, fn car -> car.seats >= group.people) do
-      [] -> "waiting"
-      vehicle ->  Enum.min_by(vehicle, fn v -> v.seats end)
-    end
+  def find_car(group_id) when is_integer(group_id) do
+    cars = get_all_cars()
+
+
+    case record_exists?(:group_cache, group_id) do
+      nil -> "no record"
+      group -> 
+        case Enum.filter(cars, fn car -> car.seats >= group.people end) do
+          [] -> "waiting"
+          vehicle ->  Enum.min_by(vehicle, fn v -> v.seats end) 
+        end
+    end  
   end
 
-  def find_car(group, cars), when is_integer(group.id) do
-    "no record"
-  end
-
-  def find_car(group, cars) do
+  def find_car(group_id) do
     "invalid payload"
   end
 
@@ -99,9 +103,13 @@ defmodule CarpoolApi do
   """
 
   def record_exists?(table, key) do
-    CarpoolApi.Cache.get(table, key)
+    Cache.get(table, key)
   end
 
+  def get_all_cars do
+    Cache.get(:cars_cache, :cars)
+    |> Enum.map(fn car -> convert_keys_to_atoms(car) end)
+  end
   @doc """
   Converts map with string keys to atom keys
   """
